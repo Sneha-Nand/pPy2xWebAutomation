311735dd7d226269a03951e4a57f3336775c7ffc
Sneha-Nand <jnsneha04@gmail.com>
2024-04-27 17:01:24 +0530

    Selenium Mini project 2

--- src/22042024/MiniProject3.py
+++ src/22042024/MiniProject3.py
@@ -1,5 +1,7 @@
 import time
 
+import allure
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -11,27 +13,31 @@ def test_Error():
     time.sleep(10)
     
     # switch to frame
-    driver.switch_to.frame(driver.find_element(By.ID,"result"))
+    driver.switch_to.frame(driver.find_element(By.ID, "result"))
     
     # Enter details for email
-    email = driver.find_element(By.XPATH,"//input[@id='email']")
+    email = driver.find_element(By.XPATH, "//input[@id='email']")
     email.send_keys("test@test.com")
     
     # Enter details for Password
-    password = driver.find_element(By.XPATH,"//input[@id='password']")
+    password = driver.find_element(By.XPATH, "//input[@id='password']")
     password.send_keys("1234567")
     
     # Enter details for Confirm Password
-    conPassword = driver.find_element(By.XPATH,"//input[@id='password2']")
+    conPassword = driver.find_element(By.XPATH, "//input[@id='password2']")
     conPassword.send_keys("1234567")
     
     # Click on submit button
-    btn = driver.find_element(By.XPATH,"//button[contains(text(),'Submit')]")
+    btn = driver.find_element(By.XPATH, "//button[contains(text(),'Submit')]")
     btn.click()
     
+    time.sleep(3)
+    
+    # take screenshot
+    allure.attach(driver.get_screenshot_as_png(), name='Error_SS', attachment_type=AttachmentType.PNG)
+    
     # find error messsage
-    err_msg = driver.find_element(By.XPATH,"//div[@class='form-control error']/child::small")
+    err_msg = driver.find_element(By.XPATH, "//div[@class='form-control error']/child::small")
     assert err_msg.text == "Username must be at least 3 characters"
     
-    
-    time.sleep(3)
+    driver.quit()
